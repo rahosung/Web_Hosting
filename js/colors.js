f1e0e21cc34bc7385f06fe13dd161ecf05ce0c9e
rahosung <110014839+rahosung@users.noreply.github.com>
2023-04-02 20:02:27 +0900

    Split into JavaScript files

---
+++ js/colors.js
@@ -0,0 +1,36 @@
+let Links = {
+    setColor: function (color) {
+        let alist = document.querySelectorAll('a');
+        let i = 0;
+        while (i < alist.length) {
+            console.log(alist[i]);
+            alist[i].style.color = color;
+            i++
+        }
+    }
+}
+let Body = {
+    setColor: function (color) {
+        document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor: function (color) {
+        document.querySelector('body').style.background = color;
+    }
+};
+
+function nightDayHandler(self) {
+    let target = document.querySelector('body');
+    if (self.value === 'night') {
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value = 'day';
+
+        Links.setColor('white');
+    } else {
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        self.value = 'night';
+
+        Links.setColor('blue');
+    }
+}
